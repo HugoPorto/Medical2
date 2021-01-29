dc032547b055103a48413facaa9f74f88b4b06c0
HugoPorto <victor.almeida.porto7@gmail.com>
2021-01-29 04:52:58 -0300

    Update tela médico

--- src/main/java/me/medical/screens/Medico.java
+++ src/main/java/me/medical/screens/Medico.java
@@ -12,8 +12,8 @@ import javax.swing.JMenuItem;
 
 public class Medico {
 	private JFrame frame;
-	
-	private JLabel lblBackground;	
+
+	private JLabel lblBackground;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -41,24 +41,76 @@ public class Medico {
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
+	}
+
+	private void initializeMenubar() {
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
+
+		this.initializeItensOfMenu(menuBar);
+	}
+	
+	private void initializeItensOfMenu(JMenuBar menuBar) {
+		this.initializeMenuOfPatients(menuBar);
+		this.initializeMenuEvolutionPatients(menuBar);
+		this.initializeMenuPrescription(menuBar);
+		this.initializeMenuCertificates(menuBar);
+		this.initializeMenuReports(menuBar);
+	}
+	
+	private void initializeMenuOfPatients(JMenuBar menuBar) {
+		JMenu mnMenuPatient = new JMenu("Pacientes");
+		mnMenuPatient.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnMenuPatient);
+
+		JMenu mnMenuPatientCare = new JMenu("Atendimentos dos Pacientes");
+		mnMenuPatientCare.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		mnMenuPatient.add(mnMenuPatientCare);
+
+		JMenuItem mnPatientsList = new JMenuItem("Listar Atendimentos");
+		mnPatientsList.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		mnMenuPatientCare.add(mnPatientsList);
+
+		JMenuItem mnPatientRegister = new JMenuItem("Cadastrar Atendimento");
+		mnPatientRegister.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		mnMenuPatientCare.add(mnPatientRegister);
+
+		JMenuItem mnHistoryPatient = new JMenuItem("Hist\u00F3riocos");
+		mnHistoryPatient.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		mnMenuPatient.add(mnHistoryPatient);
+	}
+	
+	private void initializeMenuPrescription(JMenuBar menuBar) {
+		JMenu mnMenuPrescription = new JMenu("Receituário");
+		mnMenuPrescription.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnMenuPrescription);
+	}
+	
+	private void initializeMenuCertificates(JMenuBar menuBar) {
+		JMenu mnMenuCertificates= new JMenu("Atestados");
+		mnMenuCertificates.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnMenuCertificates);
+	}
+	
+	private void initializeMenuReports(JMenuBar menuBar) {
+		JMenu mnMenuReports = new JMenu("Laudos");
+		mnMenuReports.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnMenuReports);
+	}
+	
+	private void initializeMenuEvolutionPatients(JMenuBar menuBar) {
+		JMenu mnMenuEvolution = new JMenu("Evolu\u00E7\u00E3o do Paciente");
+		mnMenuEvolution.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnMenuEvolution);
 		
-		JMenu mnNewMenu = new JMenu("File");
-		menuBar.add(mnNewMenu);
+		JMenuItem mnMenuEvolutionRegister = new JMenuItem("Cadastrar Evolução do Paciente");
+		mnMenuEvolutionRegister.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		mnMenuEvolution.add(mnMenuEvolutionRegister);
 		
-		JMenu mnNewMenu_1 = new JMenu("Home");
-		mnNewMenu.add(mnNewMenu_1);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("HOme2");
-		mnNewMenu_1.add(mntmNewMenuItem);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Home3");
-		mnNewMenu_1.add(mntmNewMenuItem_1);
-		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Save");
-		mnNewMenu.add(mntmNewMenuItem_2);
+		JMenuItem mnEvolutionPatientsList = new JMenuItem("Ver Evolução dos Pacientes");
+		mnEvolutionPatientsList.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		mnMenuEvolution.add(mnEvolutionPatientsList);
 	}
 
 	private void initializeTextFields() {
@@ -74,16 +126,16 @@ public class Medico {
 		lblBackground = new JLabel();
 		lblBackground.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblBackground.setBounds(0, 0, 1382, 724);
-		lblBackground
-				.setIcon(new javax.swing.ImageIcon(getClass().getResource("/me/medical/images/FUNDO_MEDICO.png")));
+		lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/me/medical/images/FUNDO_MEDICO.png")));
 		frame.getContentPane().add(lblBackground);
 	}
-	
+
 	private void initializeDB() {
 	}
 
 	private void initialize() {
 		this.initializeFrame();
+		this.initializeMenubar();
 		this.initializeButtons();
 		this.initializeLabels();
 		this.initializeTextFields();
